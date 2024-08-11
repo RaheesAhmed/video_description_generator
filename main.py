7ecc77fa385c4db77283b20b729afeaed74b2988
Rahees Ahmed <raheesahmed256@gmail.com>
2024-08-12 02:58:46 +0500

    Added:Video transaltion added

---
+++ main.py
@@ -0,0 +1,80 @@
+from IPython.display import display, Image, Audio
+
+import cv2  # We're using OpenCV to read video, to install !pip install opencv-python
+import base64
+import time
+from openai import (
+    OpenAI,
+)  # We're using OpenAI to create transaltion, to install !pip install openai
+import os
+import requests  # install !pip install requests
+
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
+# Using GPT's visual capabilities to get a description of a video
+video = cv2.VideoCapture("data/bison.mp4")
+
+base64Frames = []
+while video.isOpened():
+    success, frame = video.read()
+    if not success:
+        break
+    _, buffer = cv2.imencode(".jpg", frame)
+    base64Frames.append(base64.b64encode(buffer).decode("utf-8"))
+
+video.release()
+print(len(base64Frames), "frames read.")
+
+
+PROMPT_MESSAGES = [
+    {
+        "role": "user",
+        "content": [
+            "These are frames from a video that I want to upload. Generate a compelling description that I can upload along with the video.",
+            *map(lambda x: {"image": x, "resize": 768}, base64Frames[0::50]),
+        ],
+    },
+]
+params = {
+    "model": "gpt-4o",
+    "messages": PROMPT_MESSAGES,
+    "max_tokens": 200,
+}
+
+result = client.chat.completions.create(**params)
+print(result.choices[0].message.content)
+
+PROMPT_MESSAGES = [
+    {
+        "role": "user",
+        "content": [
+            "These are frames of a video. Create a short voiceover script in the style of David Attenborough. Only include the narration.",
+            *map(lambda x: {"image": x, "resize": 768}, base64Frames[0::60]),
+        ],
+    },
+]
+params = {
+    "model": "gpt-4o",
+    "messages": PROMPT_MESSAGES,
+    "max_tokens": 500,
+}
+
+result = client.chat.completions.create(**params)
+print(result.choices[0].message.content)
+
+response = requests.post(
+    "https://api.openai.com/v1/audio/speech",
+    headers={
+        "Authorization": f"Bearer {os.environ['OPENAI_API_KEY']}",
+    },
+    json={
+        "model": "tts-1-1106",
+        "input": result.choices[0].message.content,
+        "voice": "onyx",
+    },
+)
+
+audio = b""
+for chunk in response.iter_content(chunk_size=1024 * 1024):
+    audio += chunk
+Audio(audio)
